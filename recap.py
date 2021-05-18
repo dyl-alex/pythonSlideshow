5454636e7ef9bc598385475fe0ac8c3322728c78
Dylan <dyalexander@comcast.net>
2021-05-18 14:58:10 -0400

    image added

--- recap.py
+++ recap.py
@@ -1,26 +1,33 @@
-from tkinter import Button, Label, Tk, mainloop
+from tkinter import *
+from PIL import ImageTk, Image
 
+count = 0
 imageList = []
 
 def addImage(img):
     imageList.append(img)
+def chooseImage():
+    count += 1
 
-addImage("image1.png")
-addImage("image2.jpg")
-addImage("image3.jpg")
-addImage("image4.jpg")
-addImage("image5.jpg")
+addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image1.png")
+addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image2.png")
+addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image3.png")
+addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image4.png")
+addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image5.png")
 
 master = Tk()
-master.geometry("800x600")
+master.geometry("600x375")
 
+back = Button(master, text="Back", height = 20 , width=20)
+next = Button(master, text="Next", height = 20 , width=20,command = chooseImage)
+back.grid(column=0, row=1)
+next.grid(column=2, row=1)
 
-back = Button(master, text="Back", height = 20 , width=20).pack(side="left")
-next = Button(master, text="Next", height = 20 , width=20).pack(side="right")
+label = Label(master, text="Python Slideshow")
+label.grid(column=1, row=0)
 
+img = PhotoImage(file=imageList[count])
 
-label = Label(master, text="Python Slideshow").pack()
-
+slideshow = Label(master, image=img,)
+slideshow.grid(column=1,row = 1)
 mainloop()
-
-fart = 1
