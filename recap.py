0e942fc8db6b68d325b446144807b4ae01fb49e7
Dylan <dyalexander@comcast.net>
2021-05-19 12:21:31 -0400

    tkinter suck

--- recap.py
+++ recap.py
@@ -1,33 +1,49 @@
-from tkinter import *
+import tkinter as tk
 from PIL import ImageTk, Image
+import itertools
 
-count = 0
-imageList = []
+imageList = ["C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image1.png",
+ "C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image2.png", 
+ "C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image3.png",
+ "C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image4.png",
+ "C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image5.png"]
+
+ #Changes list into an iterator
+imageList = itertools.cycle(imageList)
+
+#Creates window
+master = tk.Tk()
+master.geometry("600x375")
+
+panel = tk.Label(master)
+
+
 
 def addImage(img):
     imageList.append(img)
-def chooseImage():
-    count += 1
 
-addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image1.png")
-addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image2.png")
-addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image3.png")
-addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image4.png")
-addImage("C:\\Users\\dyale\\OneDrive\\Documents\\Summer Projects\\Project1\\image5.png")
+def changeImageF():
+    try:
+        img = next(imageList)
+    except(StopIteration):
+        return
+    
+    img = Image.open(img)
+    img = ImageTk.PhotoImage(img)
+    panel.img = img
+    panel['image'] = img
 
-master = Tk()
-master.geometry("600x375")
 
-back = Button(master, text="Back", height = 20 , width=20)
-next = Button(master, text="Next", height = 20 , width=20,command = chooseImage)
+back = tk.Button(master, text="Back", height = 20 , width=20)
+past = tk.Button(master, text="Next", height = 20 , width=20, command = changeImageF)
 back.grid(column=0, row=1)
-next.grid(column=2, row=1)
+past.grid(column=2, row=1)
 
-label = Label(master, text="Python Slideshow")
+label = tk.Label(master, text="Python Slideshow")
 label.grid(column=1, row=0)
 
-img = PhotoImage(file=imageList[count])
+panel.grid(column=1,row = 1)
 
-slideshow = Label(master, image=img,)
-slideshow.grid(column=1,row = 1)
-mainloop()
+changeImageF()
+
+master.mainloop()
